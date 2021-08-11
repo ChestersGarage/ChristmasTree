6b187c1691db4191d11026849c804572dd14a3e4
Mark Chester <mark@chesterfamily.org>
2021-08-11 13:40:39 -0700

    Edits

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 git commit -am "Edits"
 git push
 
-ssh pi@192.168.1.125 "git pull -c /home/pi/ChristmasTree; killall python3; nohup python3 /home/pi/ChristmasTree/flickering_candles.py > /home/pi/flickering_candles.log 2>&1 &"
+ssh pi@192.168.1.125 "git pull -C /home/pi/ChristmasTree; killall python3; nohup python3 /home/pi/ChristmasTree/flickering_candles.py > /home/pi/flickering_candles.log 2>&1 &"
