d3e8438cb76f56453f9583181e9c331c352ed847
Mark Chester <mark@chesterfamily.org>
2021-08-11 14:28:49 -0700

    Improving things...

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ git push
 
 ssh pi@192.168.1.125 "git -C /home/pi/ChristmasTree pull; killall -q python3"
 ssh pi@192.168.1.125 "nohup python3 /home/pi/ChristmasTree/${SCENE}.py > /home/pi/${SCENE}.log 2>&1 &"
-ssh pi@192.168.1.125 "nohup python3 /home/pi/ChristmasTree/tree_star.py > /home/pi/tree_star.log 2>&1"
+ssh pi@192.168.1.125 "nohup python3 /home/pi/ChristmasTree/tree_star.py > /home/pi/tree_star.log 2>&1 &"
