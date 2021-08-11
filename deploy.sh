d91b32f5e6063b100f76b5dc3b02f90808d977d6
Mark Chester <mark@chesterfamily.org>
2021-08-11 14:00:00 -0700

    Improving things...

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+SCENE=$1
+
+if [[ -z $SCENE ]]; then
+    echo "No scene specified, using default (flickering_candles)"
+    SCENE="flickering_candles"
+fi
+
 git commit -am "Improving things..."
 git push
 
-ssh pi@192.168.1.125 "git -C /home/pi/ChristmasTree pull; killall python3; nohup python3 /home/pi/ChristmasTree/flickering_candles.py > /home/pi/flickering_candles.log 2>&1 &"
+ssh pi@192.168.1.125 "git -C /home/pi/ChristmasTree pull; killall python3; nohup python3 /home/pi/ChristmasTree/${SCENE}.py > /home/pi/${SCENE}.log 2>&1 &"
