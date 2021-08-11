833acedf260f6d313687d3b5006a9f26409b70cc
Mark Chester <mark@chesterfamily.org>
2021-08-11 13:42:40 -0700

    Improving things...

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git commit -am "Edits"
+git commit -am "Improving things..."
 git push
 
 ssh pi@192.168.1.125 "git -C /home/pi/ChristmasTree pull; killall python3; nohup python3 /home/pi/ChristmasTree/flickering_candles.py > /home/pi/flickering_candles.log 2>&1 &"
