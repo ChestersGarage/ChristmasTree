dde46f5eadd65c9f6a51b8b654c53478981d3f7c
Mark Chester <mark@chesterfamily.org>
2021-08-11 17:03:27 -0700

    Improving things...

--- ever_fade.py
+++ ever_fade.py
@@ -2,47 +2,27 @@ import opc, time, math, random
 
 client = opc.Client('christmastree.home:7890')
 
-"""
 colors = [
-    ( 255, 0, 0 ),
-    ( 255, 64, 0 ),
-    ( 255, 255, 0 ),
-    ( 127, 255, 0 ),
-    ( 0, 255, 0 ),
-    ( 0, 255, 127 ),
-    ( 0, 255, 255 ),
-    ( 0, 127, 255 ),
-    ( 0, 0, 255 ),
-    ( 127, 0, 255 ),
-    ( 255, 0, 255 ),
-    ( 255, 0, 64 )
-]
-# 12 colors
-"""
-colors = [
-    ( 255, 0, 0 ),
-    ( 0, 0, 255 ),
-    ( 255, 192, 0 ),
-    ( 127, 0, 127 ),
-    ( 255, 92, 0 ),
-    ( 0, 255, 0 )
+    ( 255,   0,   0 ),
+    (   0, 255,   0 ),
+    (   0,   0, 255 ),
+    ( 192, 128,   0 ),
+    ( 192,   0, 192 ),
+    ( 192,   0, 192 )
 ]
 # 6 colors
 
 print('Starting LED Christmas tree scene: Ever-Fade.')
 
-stringLength = 265
+stringLength = 200
+fadeTime = 5
 ledString = [ [0,0,0] ] * stringLength
-client.put_pixels(ledString)
-time.sleep(5)
 
 while True:
-    for pixel in ledString:
-        ledString[pixel] = colors[random.randrange(len(colors))]
-        while ledString[pixel] == ledString[pixel-1]:
-            ledString[pixel] = colors[random.randrange(len(colors))]
+    while p < stringLength:
+        ledString[p] = colors[random.randrange(len(colors))]
+        while ledString[p] == ledString[p-1]:
+            ledString[p] = colors[random.randrange(len(colors))]
 
-    client.put_pixels(ledString)
-    time.sleep(5)
-    client.put_pixels(ledString)
-    time.sleep(5)
+    client.put_pixels(ledString,0)
+    time.sleep(fadeTime)
