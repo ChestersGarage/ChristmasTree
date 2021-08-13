fb058bf9f83cf09ced5d43a6cf4a99c6595191bb
Mark Chester <mark@chesterfamily.org>
2021-08-13 00:04:52 -0700

    Improving things...

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,6 +1,9 @@
-import opc, time, math, random
+import opc, time, math, random, tree_star
+
+class TreeScene():
 
 client = opc.Client('christmastree.home:7890')
-
+star = tree_star.Star()
+tree =
             #print('Starting LED Christmas tree scene: Tree Star.')
 
