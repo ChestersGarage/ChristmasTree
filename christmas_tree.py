bd9949cb3adcca08164d06ceb26cc27ffd2c50be
Mark Chester <mark@chesterfamily.org>
2021-08-13 09:59:31 -0700

    Improving things...

--- christmas_tree.py
+++ christmas_tree.py
@@ -13,25 +13,26 @@ _xmas_tree_scene = tree_scene_list[0]
 _step_period = 1/30
 _star_pixel_count = 65
 """Star is broken into two sets of LEDs: the edges (45px) and the folds(20px)."""
+_star_pixel_segments = [ 45, 20 ]
 _tree_pixel_count = 200
 """Tree is broken into 4 strings of 50 LEDs."""
+_tree_pixel_segments = [ 50, 50, 50, 50 ]
 
-class SetScene(object):
+def set_scene(scene):
     """
     Select which scene to run on the tree, or the default.
     """
-    def __init__(self,scene):
-        self.scene = __import__(scene)
+    scene_module = __import__(scene)
 
-        return self.scene
+    return scene_module
 
 xmas_tree = opc.Client('christmastree.home:7890')
 
-star_scene = SetScene(_xmas_star_scene)
-star = star_scene.Scene(_star_pixel_count,_step_period,_star_pixel_edge_count,_star_pixel_fold_count)
+star_scene = set_scene(_xmas_star_scene)
+star = star_scene.Scene(_star_pixel_count, _step_period, _star_pixel_segments)
 
-tree_scene = SetScene(_xmas_tree_scene)
-tree = tree_scene.Scene(_tree_pixel_count,_step_period)
+tree_scene = set_scene(_xmas_tree_scene)
+tree = tree_scene.Scene(_tree_pixel_count, _step_period, _tree_pixel_segments)
 #        star.start_msg() = 'Servicing ' + str(self.total_pixels) + ' LED pixels.'
             #print('Starting LED Christmas tree scene: Tree Star.')
 star.startup_msg()
