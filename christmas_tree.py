710278f48d3a1b04f256e66742802b3ed9522614
Mark Chester <mark@chesterfamily.org>
2021-08-12 23:17:11 -0700

    Improving things...

---
+++ christmas_tree.py
@@ -0,0 +1,6 @@
+import opc, time, math, random
+
+client = opc.Client('christmastree.home:7890')
+
+            #print('Starting LED Christmas tree scene: Tree Star.')
+
--- tree_star.py
+++ tree_star.py
@@ -1,35 +1,29 @@
-import opc, time, math, random
+class Star:
+    def __init__:
+        # 4700K, "White" and 9800K
+        self.colors = [
+            ( 255, 223, 194 ),
+            ( 240, 240, 240 ),
+            ( 206, 220, 255 )
+            ]
+        self.totalPixels = 65
+        self.frameRate = 1 / 10
+        self.ledString = [ [0,0,0] ] * totalPixels
+        self.startMsg = 'Servicing ' + str(totalPixels) + ' LED pixels.'
 
-client = opc.Client('christmastree.home:7890')
+    def __main__():
+        numPixToChange = int(totalPixels / 5)
+        if numPixToChange <= 1:
+            numPixToChange = 1
+        else:
+            numPixToChange = random.randrange(numPixToChange * 2)
 
-# 4700K, "White" and 9800K
-colors = [
-    ( 255, 223, 194 ),
-    ( 240, 240, 240 ),
-    ( 206, 220, 255 )
-    ]
+        p = 0
+        while p < numPixToChange:
+            brightness = random.randrange(60,80) / 100.0
+            hue = colors[random.randrange(len(colors))]
+            ledString[random.randrange(totalPixels)] = [ hue[0] * brightness, hue[1] * brightness, hue[2] * brightness ]
+            p += 1
 
-print('Starting LED Christmas tree scene: Tree Star.')
-
-totalPixels = 65
-frameRate = 1 / 10
-ledString = [ [0,0,0] ] * totalPixels
-
-print('Servicing ' + str(totalPixels) + ' LED pixels.')
-
-numPixToChange = int(totalPixels / 5)
-if numPixToChange <= 1:
-    numPixToChange = 1
-else:
-    numPixToChange = random.randrange(numPixToChange * 2)
-
-while True:
-    p = 0
-    while p < numPixToChange:
-        brightness = random.randrange(60,80) / 100.0
-        hue = colors[random.randrange(len(colors))]
-        ledString[random.randrange(totalPixels)] = [ hue[0] * brightness, hue[1] * brightness, hue[2] * brightness ]
-        p += 1
-
-    client.put_pixels(ledString,2)
-    time.sleep(frameRate)
+        #client.put_pixels(ledString,2)
+        return ledString
