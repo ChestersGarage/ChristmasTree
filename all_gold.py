e5e66f03a917baa11a1b33b78114873b4d48c89b
Mark Chester <mark@chesterfamily.org>
2021-08-14 01:03:39 -0700

    Improving things...

--- all_gold.py
+++ all_gold.py
@@ -6,7 +6,7 @@ class Scene(object):
     def __init__(self, step_period, pixel_count):
         # ~4700K, "White" and ~9800K
         self._colors = [
-            ( 255, 255, 255 )
+            ( 255, 192, 0 )
             ]
         self._pixel_count = pixel_count
         self._step_period = step_period
