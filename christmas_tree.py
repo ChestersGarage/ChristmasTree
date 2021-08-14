f94cf4d703467ebbe5fe74dc18cfe93517da5a69
Mark Chester <mark@chesterfamily.org>
2021-08-13 23:20:06 -0700

    Improving things...

--- christmas_tree.py
+++ christmas_tree.py
@@ -9,10 +9,10 @@ The tree has 4 strings of 50 LEDs, which would be placed on the tree like regula
 import opc, math, random
 from time import monotonic_ns,sleep
 
-xmas_tree_address = 'christmastree.home:7890'
-
-# These two next vars need to be provided as a JSON config file.
-scene_list = {
+# These three next vars need to be provided as a JSON config file.
+_xmas_tree_address = 'christmastree.home:7890'
+# The list of available scenes and what the
+_scene_list = {
     "old_skool_string":   "both",
     "flickering_candles": "tree",
     "ever_fade":          "both",
@@ -20,43 +20,41 @@ scene_list = {
     "all_white":          "both",
     "all_gold":           "both"
     }
-
 # Star has two sets of LEDs: the edges (45px) and the folds(20px) for aesthetic reasons.
 # Tree has 4 strings of 50 LEDs for technical reasons.
 # BTW, this config matches how the LEDs are connected to the Fadecandy board.
-led_layout = {
-    "segments": [ "star_edge", "star_fold", "tree_1", "tree_2", "tree_3", "tree_4" ],
+_led_layout = {
+    "segments": [ "tree_1", "tree_2", "tree_3", "tree_4", "star_edge", "star_fold" ],
     "tree_1_count":     "50",
     "tree_2_count":     "50",
     "tree_3_count":     "50",
     "tree_4_count":     "50",
     "star_edge_count" : "45",
     "star_fold_count":  "20",
-    "tree_1_scene":    scene_list["old_skool_string"],
-    "tree_2_scene":    scene_list["old_skool_string"],
-    "tree_3_scene":    scene_list["old_skool_string"],
-    "tree_4_scene":    scene_list["old_skool_string"],
-    "star_edge_scene": scene_list["all_white"],
-    "star_fold_scene": scene_list["all_gold"]
+    "tree_1_scene":    "old_skool_string",
+    "tree_2_scene":    "old_skool_string",
+    "tree_3_scene":    "old_skool_string",
+    "tree_4_scene":    "old_skool_string",
+    "star_edge_scene": "all_white",
+    "star_fold_scene": "all_gold"
     }
-
-# LED layout: 50, 50, 50, 50, 45, 20
-led_colors = [ [0,0,0] ] * ( _tree_1_count * 4 + _star_edge_count + _star_fold_count )
 
 # Default to 30 FPS, in nanoseconds
 _step_period = 1/30*1000000000
 _step_last_update = monotonic_ns()
 
+# LED layout: 50, 50, 50, 50, 45, 20
+led_colors = [ [0,0,0] ] * ( (tree_1_count * 4) + star_edge_count + star_fold_count )
+
+
 # Begin
-xmas_tree = opc.Client(xmas_tree_address)
+xmas_tree = opc.Client(_xmas_tree_address)
 
-star_scene = __import__(_xmas_star_scene)
-star = star_scene.Scene(_step_period, _star_pixel_count, _star_pixel_segments)
-star.startup_msg('star')
-
-tree_scene = __import__(_xmas_tree_scene)
-tree = tree_scene.Scene( _step_period, _tree_pixel_count, _tree_pixel_segments)
-tree.startup_msg('tree')
+# Set up an instance of each scene for each LED segment
+for segment_label in _led_layout['segments']:
+    segment_scene = __import__( _led_layout[segment_label + '_scene'] )
+    segments[segment_label] = segment_scene.Scene( _step_period, _led_layout[segment_label + '_count'] )
+    segment.startup_msg(segment_label)
 
 while True:
     led_string = tree.led_values() + star.led_values()
