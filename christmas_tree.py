288037735e65134e9e6f112cb09cc32497766398
Mark Chester <mark@chesterfamily.org>
2021-08-13 17:21:27 -0700

    Improving things...

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,5 +1,5 @@
 import opc, math, random
-from time import monotonic_ns
+from time import monotonic_ns,sleep
 
 xmas_tree_address = 'christmastree.home:7890'
 
@@ -29,26 +29,28 @@ led_string = [ [0,0,0] ] * (_tree_pixel_count + _star_pixel_count )
 _step_period = 1/30*1000000000
 _step_last_update = monotonic_ns()
 
-def set_scene(scene):
-    """    Select which scene to run on the tree.    """
-    return __import__(scene)
-
 
 # Begin
 xmas_tree = opc.Client(xmas_tree_address)
 
-star_scene = set_scene(_xmas_star_scene)
-star = star_scene.Scene(_star_pixel_count, _step_period, _star_pixel_segments)
+star_scene = __import__(_xmas_star_scene)
+star = star_scene.Scene(_star_pixel_count, _star_pixel_segments, _step_period)
 star.startup_msg()
 
-tree_scene = set_scene(_xmas_tree_scene)
-tree = tree_scene.Scene(_tree_pixel_count, _step_period, _tree_pixel_segments)
+tree_scene = __import__(_xmas_tree_scene)
+tree = tree_scene.Scene(_tree_pixel_count, _tree_pixel_segments, _step_period)
 tree.startup_msg()
 
 while True:
     led_string = tree.led_values() + star.led_values()
-    while monotonic_ns() < ( _step_last_update + _step_period ):
-        continue
+
+    if monotonic_ns() < ( _step_last_update + _step_period ):
+        # Sleep for however long we have left until next LED string update.
+        sleep( ( _step_last_update + _step_period ) - monotonic_ns() )
+    else:
+        overshoot = monotonic_ns() - ( _step_last_update + _step_period )
+        print('Took too long to process LED string values.')
+        print('Increase _step_period by ' + overshoot + ' nanoseconds.')
 
     _step_last_update = monotonic_ns()
     xmas_tree.put_pixels(led_string,0)
