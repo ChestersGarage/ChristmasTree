2cf3b98f0fa6c98469228f251e77b6cf8fbd0462
Mark Chester <mark@chesterfamily.org>
2021-08-14 00:40:46 -0700

    Improving things...

---
+++ all_white.py
@@ -0,0 +1,21 @@
+class Scene(object):
+    """
+    Twinkling warm, neutral and cool white pixels.
+    Sends fresh new values at each step_period.
+    """
+    def __init__(self, step_period, pixel_count):
+        # ~4700K, "White" and ~9800K
+        self._colors = [
+            ( 255, 255, 255 )
+            ]
+        self._pixel_count = pixel_count
+        self._step_period = step_period
+        self._led_colors = [ [255,255,255] ] * self._pixel_count
+
+    def led_values(self):
+        self._led_colors = [ [255,255,255] ] * self._pixel_count
+
+        return self.led_colors
+
+    def startup_msg(self,segment):
+        print('Running scene "all_white" on segment "' + segment + '".')
