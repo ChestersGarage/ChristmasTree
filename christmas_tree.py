1f304c49f97f7ac1866dbc86ad44f375acc3bceb
Mark Chester <mark@chesterfamily.org>
2021-12-14 08:14:13 +0000

    Improving things...

--- christmas_tree.py
+++ christmas_tree.py
@@ -43,8 +43,8 @@ while True:
             #print(desat_incr)
             desat_led_values.extend([
                 int(led_value[0] + desat_incr if (led_value[0] + desat_incr) <= 255 else 255),
-                int(led_value[1] + (desat_incr/2) if (led_value[1] + desat_incr) <= 255 else 255),
-                int(led_value[2] + (desat_incr/3) if (led_value[2] + desat_incr) <= 255 else 255)
+                int(led_value[1] + (int(desat_incr*0.85)) if (led_value[1] + (int(desat_incr*0.85))) <= 255 else 255),
+                int(led_value[2] + (int(desat_incr*0.75)) if (led_value[2] + (int(desat_incr*0.75))) <= 255 else 255)
 
             ])
             #print(desat_led_values)
